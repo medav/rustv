c00405a5c326f54104269fdfe7e50eef51d06584
Michael Davies <michaelstoby@gmail.com>
2023-03-11 17:36:47 -0600

    Add some debugging features

---
+++ src/disasm.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+use std::convert::TryInto;
+use std::fs::File;
+use std::io::{ self, BufRead, BufReader };
+
+fn read_lines(filename: &String) -> io::Lines<BufReader<File>> {
+    let file = File::open(filename).unwrap();
+    return io::BufReader::new(file).lines();
+}
+
+
+pub fn parse_disasm(filename : &String) -> HashMap<u64, String> {
+    let mut map = HashMap::<u64, String>::new();
+
+    for maybe_line in read_lines(filename) {
+        if let Ok(line) = maybe_line {
+            let parts = line.split(" ").collect::<Vec<_>>();
+
+            if parts.len() == 2 {
+                if let Ok(addr) = u64::from_str_radix(parts[0], 16) {
+                    let maybe_name = parts[1]
+                        .strip_suffix(">:")
+                        .unwrap_or("")
+                        .strip_prefix("<");
+
+                    if let Some(name) = maybe_name {
+                        println!("0x{:08x}: {}", addr, name);
+                        map.insert(addr, name.to_string());
+                    }
+                }
+            }
+        }
+    }
+
+
+    map
+}
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ extern crate libc;
 
 use std::fs::File;
 use std::io::Read;
+use std::collections::HashMap;
 
 mod syscalls;
 #[macro_use]
@@ -17,6 +18,7 @@ mod rv64defs;
 mod rv64alu;
 mod rv64inst;
 mod rv64emu;
+mod disasm;
 
 use memif::*;
 use bitops::*;
@@ -51,6 +53,14 @@ impl MemIf for Vec<u8> {
 fn main() {
     let filename = std::env::args().nth(1).expect("No filename provided!");
 
+    let disasm_map =
+        if let Some(disasm_file ) = std::env::args().nth(2) {
+            disasm::parse_disasm(&disasm_file)
+        }
+        else {
+            HashMap::<u64, String>::new()
+        };
+
     let mut arr : Vec<u8> = read_bin(&filename);
 
     let mut arch = ArchState {
@@ -58,20 +68,48 @@ fn main() {
         regs: [0; 32]
     };
 
+
     loop {
         let raw_inst = fetch_inst(&arch, &mut arr);
         // println!("{:04x}: {:?}", arch.pc, raw_inst);
         let decoded = decode(&raw_inst);
         // println!("{:?}", arch.regs);
-        println!("{:04x}: ({:08x}) {:?}", arch.pc, raw_inst.raw, decoded);
+
+        println!("    {:04x}: ({:08x}) {:?}", arch.pc, raw_inst.raw, decoded);
+
 
 
         let res = exec_inst(&mut arch, &mut arr, &decoded);
+
+        if let DecodedInst::Jalr {rs1 , rd, imm } = decoded {
+            if let Some(sym) = disasm_map.get(&arch.pc) {
+                println!("Call {}", sym);
+            }
+        }
+
+        if let DecodedInst::CJalr {rs1} = decoded {
+            if let Some(sym) = disasm_map.get(&arch.pc) {
+                println!("Call {}", sym);
+            }
+        }
+
+        if let DecodedInst::CJr {rs1} = decoded {
+            if rs1 == 1 {
+                println!("Return");
+            }
+        }
+
+        // if let DecodedInst::JR {rs1} = decoded {
+        //     if rs1 == 1 {
+        //         println!("Return");
+        //     }
+        // }
 
         if res == ExecResult::Trap {
             println!("{:?}", arch.regs);
             let syscall = rv64_parse_syscall(&mut arch);
             let res = syscalls::exec_syscall(&syscall, &mut arr);
+            println!("Syscall result = {}", res);
             arch.regs[10] = res as u64;
         }
         else if res == ExecResult::Halt {
